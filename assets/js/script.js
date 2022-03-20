0dc40e5f55dec34b4e4bec01575c4c97b1571738
narcpat <narcissearts@gmail.com>
2022-03-19 20:34:15 -0400

    wrote pseudocode for JS

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// When I click start button, the quiz starts with the first question and the timer starts
+
+// TODO question function
+
+// When I click to answer, I am given feedback as to whether I answered correctly or wrongly, and another question is presented (or do I create a next question button?)
+
+// TODO Question loop
+
+// When a question is answered incorrectly, 10 seconds are taken off the timer
+
+// TODO create timer function EX: https://stackoverflow.com/questions/58964755/subtract-time-from-timer-if-answer-is-wrong-creating-a-quiz-javascript
+
+// When all questions are answered or the timer reaches 0 the game is over
+
+// TODO End of Game function scenario
+
+// When the game is over, I can save my initials and score
+
+// TODO create storage function for scores and initials
